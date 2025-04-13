378da67341ddf6e9a50135f5e2ed16eac3e9d311
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2025-04-13 14:23:24 +0530

    Adjust Container layout and add Headline component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,10 +4,42 @@ import styled from 'styled-components';
 const Container =styled.div`
 height:100%;
 overflow-y:scroll;
+background :${({theme})=>theme.bg}
+padding-bottom:50px;
+display:flex;
+flex-direction:column;
+align-items:center;
+gap:20px;
+@media (max-width:768px){
+padding:6px 10px;
+}
+`;
+
+const Headline=styled.div`
+font-size:34px;
+font-weight:500;
+color:${({theme})=>theme.text_primary};
+display:flex;
+flex-direction:column;
+align-items:center;
+
+@media (max-width:600px){
+font-size:22px;
+}
+`;
+
+const Span=styled.div`
+font-size:30px;
+font-weight:500;
+color:${({theme})=>theme.secondary};
 `;
 
 const Home =()=> {
-  return <Container>home</Container>;
+  return <Container>
+    <Headline>Explore popular posts in the Community!
+        <Span>⦿ Generated with AI ⦿</Span>
+    </Headline>
+  </Container>;
      
   
 };
