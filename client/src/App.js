539c86dc1065f8e333b42af9df8f69254530bc07
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2025-04-13 12:01:40 +0530

    Implement basic routing and post creation page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,43 @@
  
 import './App.css';
+import styled, { ThemeProvider } from 'styled-components';
+import { darkTheme } from './utils/Theme';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
+import Home from './pages/Home';
+import CreatePost from './pages/CreatePost';
+
+const Container =styled.div`
+width :100%;
+height :100%;
+display:flex;
+background:${({theme})=>theme.bg};
+color :${({theme})=>theme.text_primary};
+overflow-x:hidden;
+overflow-xy:hidden;
+transition:all 0.2s ease;
+`;
+
+const Wrapper =styled.div`
+height :100%;
+position:relative;
+flex-direction:column;
+justify-content:space-between;
+flex:3;
+`;
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          
-      </header>
-    </div>
-  );
+  return  <ThemeProvider theme={darkTheme}>
+    <Container>
+      <Wrapper>
+        <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} exact/>
+          <Route path='/post' element={<CreatePost/>} exact/>
+        </Routes>
+        </BrowserRouter>
+      </Wrapper>
+    </Container>
+    </ThemeProvider>
 }
 
 export default App;
