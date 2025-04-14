360f376db11d04082132a1edef233faf851a842b
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2025-04-14 11:19:54 +0530

    change some style for better view

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,32 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 import GenerateImageForm from '../components/GenerateImageForm';
+import GenerateImageCard from '../components/GenerateImageCard';
 
- 
-
-
-const Container =styled.div`
-height:100%;
-overflow-y:scroll;
-background :${({theme})=>theme.bg};
- padding: 30px 30px;
-padding-bottom:50px;
-display:flex;
-flex-direction:column;
-align-items:center;
- justify-content: center;
-gap:20px;
-@media (max-width:768px){
-padding:6px 10px;
-}
+const Container = styled.div`
+  height: 100%;
+  overflow-y: scroll;
+  background: ${({ theme }) => theme.bg};
+  padding: 30px 30px;
+  padding-bottom: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  @media (max-width: 768px) {
+    padding: 6px 10px;
+  }
 `;
 
+const Wrapper = styled.div`
  
-
- 
-
-const Wrapper=styled.div`
-  flex:1;
   width: 100%;
   height: fit-content;
   max-width: 1200px;
@@ -44,6 +38,7 @@ const CreatePost=() =>{
   <Container>
     <Wrapper>
       <GenerateImageForm/>
+      <GenerateImageCard loading />
     </Wrapper>
   </Container>
   );
