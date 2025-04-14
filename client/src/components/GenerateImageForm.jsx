0e97fd4316bff422475b856016b67e8c66e609e4
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2025-04-14 11:21:04 +0530

    Update GenerateImageForm with styled components and fix title

--- client/src/components/GenerateImageForm.jsx
+++ client/src/components/GenerateImageForm.jsx
@@ -4,46 +4,41 @@ import  Button  from './button';
 import TextInput from './TextInput';
 import { AutoAwesome, CreateRounded } from '@mui/icons-material';
 
-const Form =styled.div`
-flex:1;
-padding:16px 20px;
-display:flex;
-flex-direction:column;
-gap:9%;
-justify-Content:center;
+const Form = styled.div`
+flex: 1;
+padding: 16px 20px;
+display: flex;
+flex-direction: column;
+gap: 9%;
+justify-content: center;
 `;
-
-const Top =styled.div`
-display:flex;
-flex-direction:column;
-gap:6px;
+const Top = styled.div`
+display: flex;
+flex-direction: column;
+gap: 6px;
 `;
-
-const Tittle =styled.div`
-font-size:28px;
-font-weight:500;
-color:${({theme})=>theme.text_primary};
+const Title = styled.div`
+font-size: 28px;
+font-weight: 500;
+color: ${({ theme }) => theme.text_primary};
 `;
-
-const Desc =styled.div`
-font-size:17px;
-font-weight:400;
-color:${({theme})=>theme.text_secondary};
+const Desc = styled.div`
+font-size: 17px;
+font-weight: 400;
+color: ${({ theme }) => theme.text_secondary};
 `;
-
-const Body =styled.div`
-display:flex;
-flex-direction:column;
-gap:18px;
-font-size:12px;
-font-weight:400;
-color:${({theme})=>theme.text_secondary};
+const Body = styled.div`
+display: flex;
+flex-direction: column;
+gap: 18px;
+font-size: 12px;
+font-weight: 400;
+color: ${({ theme }) => theme.text_secondary};
 `;
-
-const Action =styled.div`
-flex:1;
-display:flex;
-gap:8px;
+const Actions = styled.div`
+flex: 1;
+display: flex;
+gap: 8px;
 `;
 
 
@@ -51,7 +46,7 @@ const  GenerateImageForm= () => {
   return (
     <Form>
       <Top>
-        <Tittle>Generate Image with  prompt</Tittle>
+        <Title>Generate Image with  prompt</Title>
         <Desc>
             Write your  prompt according to the image you want to generate!
         </Desc>
@@ -65,11 +60,11 @@ const  GenerateImageForm= () => {
          />
       ** You post the AI Generated Image to  the Community **
       </Body>
-      <Action>
+      <Actions>
        <Button text="Generate Image" flex leftIcon={<AutoAwesome/>} />
        <Button text="Post Image" flex type="secondary" leftIcon={<CreateRounded/>} />
 
-      </Action>
+      </Actions>
     </Form>
   );
 }
